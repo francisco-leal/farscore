a75aee4f75ba69c11c8d93cad48fe1bd9c2c3d4a
francisco-leal <francisco@talentprotocol.com>
2024-02-22 16:55:19 +0000

    Add leaderboard and followers image

--- app/services/generate_image.ts
+++ app/services/generate_image.ts
@@ -1,12 +1,13 @@
 import satori from "satori";
 import { join } from "path";
 import * as fs from "fs";
-import { User } from "#models/user";
+import { User } from '#models/user';
+import { Connection } from '#models/connection';
 
-export const generateImage = async (fid: number) => {
-	const user = await User.findBy("fid", fid);
-	const score = user ? user.score : 0;
-	const username = user ? user.username : "unknown";
+export const generateScoreImage = async (fid: number)  => {
+  const user = await User.findBy('fid', fid);
+  const score = user ? user.score : 0;
+  const username = user ? user.username : "unknown";
 
 	const regularFontPath = join(
 		process.cwd(),
@@ -15,80 +16,266 @@ export const generateImage = async (fid: number) => {
 	);
 	const regularFontData = fs.readFileSync(regularFontPath);
 
-	const boldFontPath = join(
-		process.cwd(),
-		"/public/font",
-		"SpaceGrotesk-SemiBold.ttf",
-	);
-	const boldFontData = fs.readFileSync(boldFontPath);
+  const boldFontPath = join(process.cwd(), "/public/font", "SpaceGrotesk-SemiBold.ttf");
+  const boldFontData = fs.readFileSync(boldFontPath);
 
-	return await satori(
-		{
-			type: "div",
-			props: {
-				children: [
-					{
-						type: "p",
-						props: {
-							children: `@${username} farcaster score`,
-							style: {
-								fontSize: 24,
-								fontFamily: "SpaceGrotesk-regular",
-								color: "white",
-								zIndex: 1,
-							},
-						},
-					},
-					{
-						type: "p",
-						props: {
-							children: `${score}`,
-							style: {
-								fontSize: 32,
-								fontFamily: "SpaceGrotesk-regular",
-								color: "white",
-								zIndex: 1,
-							},
-						},
-					},
-					{
-						type: "p",
-						props: {
-							children: `by farscore.xyz`,
-							style: {
-								fontSize: 24,
-								fontFamily: "SpaceGrotesk-SemiBold",
-								color: "white",
-								zIndex: 1,
-							},
-						},
-					},
-				],
-				style: {
-					display: "flex",
-					flexDirection: "column",
-					position: "relative",
-					width: "100%",
-					height: "100%",
-					justifyContent: "center",
-					alignItems: "center",
-					backgroundColor: "black",
-				},
-			},
-		},
-		{
-			width: 675,
-			height: 675,
-			fonts: [
-				{
-					data: regularFontData,
-					name: "SpaceGrotesk-Regular",
-				},
-				{
-					data: boldFontData,
-					name: "SpaceGrotesk-SemiBold",
-				},
-			],
-		},
-	);
-};
+  return await satori(
+    {
+      type: 'div',
+      props: {
+        children: [
+          {
+            type: 'p',
+            props: {
+              children: `@${username} farcaster score`,
+              style: {
+                fontSize: 24,
+                fontFamily: "SpaceGrotesk-regular",
+                color: "white",
+                zIndex: 1,
+              }
+            }
+          },
+          {
+            type: 'p',
+            props: {
+              children: `${score}`,
+              style: {
+                fontSize: 32,
+                fontFamily: "SpaceGrotesk-regular",
+                color: "white",
+                zIndex: 1,
+              }
+            }
+          },
+          {
+            type: 'p',
+            props: {
+              children: `by farscore.xyz`,
+              style: {
+                fontSize: 24,
+                fontFamily: "SpaceGrotesk-SemiBold",
+                color: "white",
+                zIndex: 1,
+              }
+            }
+          },
+        ],
+        style: { display: 'flex', flexDirection: "column", position: 'relative', width: "100%", height: "100%", justifyContent: "center", alignItems: "center", backgroundColor: "black"},
+      },
+    },
+    {
+      width: 675,
+      height: 675,
+      fonts: [
+        {
+          data: regularFontData,
+          name: "SpaceGrotesk-Regular"
+        },
+        {
+          data: boldFontData,
+          name: "SpaceGrotesk-SemiBold"
+        }
+      ]
+    },
+  )
+}
+
+export const generateLeaderboardImage = async ()  => {
+  const topUsers = await User.query().where('score', '>', 0).orderBy('score', 'desc').limit(5);
+
+  const regularFontPath = join(process.cwd(), "/public/font", "SpaceGrotesk-Regular.ttf");
+  const regularFontData = fs.readFileSync(regularFontPath);
+
+  const boldFontPath = join(process.cwd(), "/public/font", "SpaceGrotesk-SemiBold.ttf");
+  const boldFontData = fs.readFileSync(boldFontPath);
+
+  return await satori(
+    {
+      type: 'div',
+      props: {
+        children: [
+          ...topUsers.map((user) => ({
+            type: 'div',
+            props: {
+              children: [
+                ...(user.profilePictureUrl ? [{
+                  type: 'img',
+                  props: {
+                    src: user.profilePictureUrl,
+                    style: {
+                      width: 50,
+                      height: 50,
+                      borderRadius: '50%',
+                      marginRight: 10,
+                    }
+                  }
+                }] : []),
+                {
+                  type: 'p',
+                  props: {
+                    children: `@${user.username}: ${user.score}`,
+                    style: {
+                      fontSize: 24,
+                      fontFamily: "SpaceGrotesk-Regular",
+                      color: "white",
+                      zIndex: 1,
+                      margin: 0,
+                      display: "flex",
+                      flexDirection: "row",
+                      alignItems: "center",
+                      justifyContent: "center",
+                    }
+                  }
+                },
+              ],
+              style: {
+                display: 'flex',
+                alignItems: 'center',
+                marginBottom: 32,
+              }
+            }
+          })),
+          {
+            type: 'p',
+            props: {
+              children: `by farscore.xyz`,
+              style: {
+                fontSize: 24,
+                fontFamily: "SpaceGrotesk-SemiBold",
+                color: "white",
+                zIndex: 1,
+                marginTop: 32,
+              }
+            }
+          },
+        ],
+        style: { 
+          display: 'flex',
+          flexDirection: "column",
+          position: 'relative',
+          width: "100%",
+          height: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "black"
+        },
+      },
+    },
+    {
+      width: 675,
+      height: 675,
+      fonts: [
+        {
+          data: regularFontData,
+          name: "SpaceGrotesk-Regular"
+        },
+        {
+          data: boldFontData,
+          name: "SpaceGrotesk-SemiBold"
+        }
+      ]
+    }
+  )
+}
+
+export const generateFollowersImage = async (fid: number)  => {
+  const topUsers = await User.query().whereIn(
+    'fid',
+    Connection.query().where('target_fid', fid).select('source_fid')
+  ).orderBy('score', 'desc').limit(5)
+
+  const regularFontPath = join(process.cwd(), "/public/font", "SpaceGrotesk-Regular.ttf");
+  const regularFontData = fs.readFileSync(regularFontPath);
+
+  const boldFontPath = join(process.cwd(), "/public/font", "SpaceGrotesk-SemiBold.ttf");
+  const boldFontData = fs.readFileSync(boldFontPath);
+
+  return await satori(
+    {
+      type: 'div',
+      props: {
+        children: [
+          ...topUsers.map((user) => ({
+            type: 'div',
+            props: {
+              children: [
+                ...(user.profilePictureUrl ? [{
+                  type: 'img',
+                  props: {
+                    src: user.profilePictureUrl,
+                    style: {
+                      width: 50,
+                      height: 50,
+                      borderRadius: '50%',
+                      marginRight: 10,
+                    }
+                  }
+                }] : []),
+                {
+                  type: 'p',
+                  props: {
+                    children: `@${user.username}: ${user.score}`,
+                    style: {
+                      fontSize: 24,
+                      fontFamily: "SpaceGrotesk-Regular",
+                      color: "white",
+                      zIndex: 1,
+                      margin: 0,
+                      display: "flex",
+                      flexDirection: "row",
+                      alignItems: "center",
+                      justifyContent: "center",
+                    }
+                  }
+                },
+              ],
+              style: {
+                display: 'flex',
+                alignItems: 'center',
+                marginBottom: 32,
+              }
+            }
+          })),
+          {
+            type: 'p',
+            props: {
+              children: `by farscore.xyz`,
+              style: {
+                fontSize: 24,
+                fontFamily: "SpaceGrotesk-SemiBold",
+                color: "white",
+                zIndex: 1,
+                marginTop: 32,
+              }
+            }
+          },
+        ],
+        style: { 
+          display: 'flex',
+          flexDirection: "column",
+          position: 'relative',
+          width: "100%",
+          height: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "black"
+        },
+      },
+    },
+    {
+      width: 675,
+      height: 675,
+      fonts: [
+        {
+          data: regularFontData,
+          name: "SpaceGrotesk-Regular"
+        },
+        {
+          data: boldFontData,
+          name: "SpaceGrotesk-SemiBold"
+        }
+      ]
+    }
+  )
+}
